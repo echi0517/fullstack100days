9ccafc023601b6b1ae30aacf2cc8e8fa26546771
DESKTOP-NPLM7LE\cube <sypark>
2023-02-22 17:56:36 +0900

    틱택토 게임 만들기

---
+++ 08_section14-tictactoe/scripts/app.js
@@ -0,0 +1,15 @@
+const playerConfigOverlayElement = document.getElementById('config-overlay')
+const backdropElement = document.getElementById('backdrop')
+
+const editPlayer1BtnElement = document.querySelector('#edit-player-1-btn')
+const editPlayer2BtnElement = document.querySelector('#edit-player-2-btn')
+
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn')
+
+editPlayer1BtnElement.addEventListener('click',openPlayerConfig)
+editPlayer2BtnElement.addEventListener('click',openPlayerConfig)
+
+cancelConfigBtnElement.addEventListener('click',closePlayerConfig)
+backdropElement.addEventListener('click',closePlayerConfig)
+
+
